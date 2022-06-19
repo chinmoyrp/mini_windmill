a3718793c0e7436aa65baf7bd1ccaacfc698bc7b
Chinmoy Pradhan <chinmoyrp65@gmail.com>
2022-06-19 07:08:04 +0000

    Read from server

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -10,4 +10,4 @@ export const stepName = writable('');
 export const stepCode = writable('');
 export const stepHash = writable('');
 
-export const apiServer = readable('http://localhost:8080/api');
+export const apiServer = readable('http://172.104.234.121:8080/api');
